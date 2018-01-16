5e64c98377bb5cd5eeedfe035f7859a444376c22
朱永光 <heavenwing@msn.com>
2018-01-16 13:54:47 +0800

    add ASP.NET Core webservice

---
+++ src/WordCount.CoreWeb/CoreWeb.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Kevsoft.MassTransit.ServiceFabric;
+using MassTransit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace WordCount.CoreWeb
+{
+    /// <summary>
+    /// The FabricRuntime creates an instance of this class for each service type instance. 
+    /// </summary>
+    internal sealed class CoreWeb : StatelessService
+    {
+        public CoreWeb(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        private static IBusControl CreateBus()
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host(new Uri("rabbitmq://localhost/"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+            });
+
+            return busControl;
+        }
+
+        /// <summary>
+        /// Optional override to create listeners (like tcp, http) for this service instance.
+        /// </summary>
+        /// <returns>The collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            var bus = CreateBus();
+
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext =>
+                    new KestrelCommunicationListener(serviceContext, "ServiceEndpoint", (url, listener) =>
+                    {
+                        ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
+
+                        return new WebHostBuilder()
+                                    .UseKestrel()
+                                    .ConfigureServices(
+                                        services => services
+                                            .AddSingleton<StatelessServiceContext>(serviceContext)
+                                            .AddSingleton<IBus>(bus)
+                                            )
+                                    .UseContentRoot(Directory.GetCurrentDirectory())
+                                    .UseStartup<Startup>()
+                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
+                                    .UseUrls(url)
+                                    .Build();
+                    }),"kestrel"),
+                new ServiceInstanceListener(_ => new MassTransitListener(bus), "masstransit")
+            };
+        }
+    }
+}
---
+++ src/WordCount.CoreWeb/DefaultController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Kevsoft.WordCount.Messages;
+using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WordCount.CoreWeb
+{
+    [Produces("application/json")]
+    [Route("api/Default")]
+    public class DefaultController : Controller
+    {
+        private readonly IBus _bus;
+
+        public DefaultController(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpGet("Count")]
+        public async Task<IActionResult> Count()
+        {
+            var address = new Uri("rabbitmq://localhost/WordCount.Service");
+            var requestTimeout = TimeSpan.FromSeconds(30);
+
+            var client = new MessageRequestClient<ICountRequest, ICountResponse>(_bus, address, requestTimeout);
+
+            var response = await client.Request(new CountRequest(), CancellationToken.None).ConfigureAwait(false);
+
+            return Content($"<h1> Total: {response.Count} </h1>", "text/html", Encoding.UTF8);
+        }
+
+        [HttpPost("AddWord/{word}")]
+        public async Task<IActionResult> AddWord(string word)
+        {
+            await _bus.Publish<IAddWord>(new { Word = word }).ConfigureAwait(false);
+
+            return Content($"<h1>{word}</h1> added", "text/html", Encoding.UTF8);
+        }
+    }
+}
